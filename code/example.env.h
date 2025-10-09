b79c27439196598927230c50cefd07c07f93092e
Giovannids100 <giovannids100@gmail.com>
2025-10-09 17:17:51 +0200

    add example.env

---
+++ code/example.env.h
@@ -0,0 +1,5 @@
+//rename in env.h and write the info accordingly
+
+const char* ssid = "your_wifi_name";
+const char* password = "your_wifi_password";
+const char* serverName = "http://SERVER_IP:API_PORT/path/to/api";
